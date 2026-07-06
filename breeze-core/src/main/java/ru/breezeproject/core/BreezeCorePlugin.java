b74ce16dd45d7d9192fddb1c284456a8ec4f48cd
hxragi <mixintrace@gmail.com>
2026-07-06 11:26:09 +0300

    fix: plugin disabled if database connection is missing

--- breeze-core/src/main/java/ru/breezeproject/core/BreezeCorePlugin.java
+++ breeze-core/src/main/java/ru/breezeproject/core/BreezeCorePlugin.java
@@ -10,51 +10,56 @@ import ru.breezeproject.core.service.SimpleServiceRegistry;
 
 public final class BreezeCorePlugin extends JavaPlugin {
 
-    private DatabaseManager databaseManager;
-    private ModuleLoader moduleLoader;
-    private SimpleServiceRegistry serviceRegistry;
+  private DatabaseManager databaseManager;
+  private ModuleLoader moduleLoader;
+  private SimpleServiceRegistry serviceRegistry;
 
-    @Override
-    public void onEnable() {
-        saveDefaultConfig();
+  @Override
+  public void onEnable() {
+    saveDefaultConfig();
 
-        this.databaseManager = new DatabaseManager(getLogger());
-        try {
-            databaseManager.connect(getConfig());
-            new DatabaseMigrator(getLogger()).migrate(databaseManager.getDataSource(), databaseManager.getVendor());
-        } catch (Exception e) {
-            getLogger().severe("Could not connect to the database, disabling BreezeCore: " + e.getMessage());
-            getServer().getPluginManager().disablePlugin(this);
-            return;
-        }
+    if (getConfig().getBoolean("database.enabled", false)) {
 
-        this.serviceRegistry = new SimpleServiceRegistry();
-        this.moduleLoader = new ModuleLoader(getDataFolder(), serviceRegistry, getLogger(), this);
-        this.moduleLoader.loadAll();
-
-        PluginCommand cmd = getCommand("breezemodules");
-        if (cmd != null) {
-            cmd.setExecutor(new ModulesCommand(moduleLoader));
-        }
-
-        getLogger().info("BreezeCore enabled, " + moduleLoader.getLoadedModules().size() + " module(s) loaded.");
+      this.databaseManager = new DatabaseManager(getLogger());
+      try {
+        databaseManager.connect(getConfig());
+        new DatabaseMigrator(getLogger()).migrate(databaseManager.getDataSource(), databaseManager.getVendor());
+      } catch (Exception e) {
+        getLogger().severe("Could not connect to the database, disabling BreezeCore: " + e.getMessage());
+        getServer().getPluginManager().disablePlugin(this);
+        return;
+      }
+    } else {
+      getLogger().info("Database is disabled in config.yml. Skipping database connection and migrations.");
     }
 
-    @Override
-    public void onDisable() {
-        if (moduleLoader != null) {
-            moduleLoader.unloadAll();
-        }
-        if (databaseManager != null) {
-            databaseManager.shutdown();
-        }
+    this.serviceRegistry = new SimpleServiceRegistry();
+    this.moduleLoader = new ModuleLoader(getDataFolder(), serviceRegistry, getLogger(), this);
+    this.moduleLoader.loadAll();
+
+    PluginCommand cmd = getCommand("breezemodules");
+    if (cmd != null) {
+      cmd.setExecutor(new ModulesCommand(moduleLoader));
     }
 
-    public ModuleLoader getModuleLoader() {
-        return moduleLoader;
-    }
+    getLogger().info("BreezeCore enabled, " + moduleLoader.getLoadedModules().size() + " module(s) loaded.");
+  }
 
-    public DatabaseManager getDatabaseManager() {
-        return databaseManager;
+  @Override
+  public void onDisable() {
+    if (moduleLoader != null) {
+      moduleLoader.unloadAll();
     }
+    if (databaseManager != null) {
+      databaseManager.shutdown();
+    }
+  }
+
+  public ModuleLoader getModuleLoader() {
+    return moduleLoader;
+  }
+
+  public DatabaseManager getDatabaseManager() {
+    return databaseManager;
+  }
 }
